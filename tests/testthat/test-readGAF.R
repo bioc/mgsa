3f02cb4b304f335b5731bafe6f34547f399e44b0
Sebastian Bauer <mail@sebastianbauer.info>
2023-04-03 00:11:01 +0200

    Fix test failing due to GO.db and friends update

--- tests/testthat/test-readGAF.R
+++ tests/testthat/test-readGAF.R
@@ -1,9 +1,9 @@
 test_that("readGAF() works", {
   d<-readGAF("gene_association_head.goa_ref_human.gz")
-  expect_equal(d@numberOfItems, 182)
+  expect_equal(d@numberOfItems, 159)
 })
 
 test_that("readGAF() with aspect works", {
   d<-readGAF("gene_association_head.goa_ref_human.gz",aspect="C")
-  expect_equal(d@numberOfItems, 149)
+  expect_equal(d@numberOfItems, 119)
 })
